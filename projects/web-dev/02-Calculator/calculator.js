0cd30146004278e227bd7709e1e44ad59a6e8226
Marta <msieroslawska@users.noreply.github.com>
2019-05-03 19:48:47 +0200

    [Vanilla calculator] Create numbers from digits

--- projects/web-dev/02-Calculator/calculator.js
+++ projects/web-dev/02-Calculator/calculator.js
@@ -1,10 +1,45 @@
 /* eslint-disable no-undef */
 
-const result = document.querySelector('.result');
+const resultElement = document.querySelector('.result');
+
+const isDigit = value => !Number.isNaN(parseInt(value, 10));
+
+const handleDigit = (value) => {
+  const currentValue = resultElement.innerHTML;
+
+  resultElement.innerHTML = currentValue.startsWith('0')
+    ? value
+    : `${currentValue}${value}`;
+};
+
+const handleNonDigit = (value) => {
+  switch (value) {
+    case 'C':
+      resultElement.innerHTML = '0';
+      break;
+    case '<-':
+      break;
+    case '=':
+      break;
+    case '+':
+      break;
+    case '-':
+      break;
+    case 'x':
+      break;
+    case '/':
+      break;
+    default:
+    //
+  }
+};
 
 const handleButtonClick = (value) => {
-  console.log(value);
-  result.innerHTML = value;
+  if (isDigit(value)) {
+    handleDigit(value);
+  } else {
+    handleNonDigit(value);
+  }
 };
 
 document.querySelector('.main')
