2e545788523a70966d6b5e16b39b4c599a70da1f
Marta <msieroslawska@users.noreply.github.com>
2020-07-12 18:13:34 +0200

    chore: regen site

---
+++ docs/_next/static/0w0nYyKt6x7qkskEdCKLT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/articles":[a],"/articles/[slug]":[a],"/codelogs":[a],"/codelogs/[slug]":[a],"/index":[a],"/projects":[a],"/todo":[a]}}("static\u002Fchunks\u002F5aa084ec7be7831c24a3cfa412b67eb3bf512934.9d410b191eaed73e5094.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
