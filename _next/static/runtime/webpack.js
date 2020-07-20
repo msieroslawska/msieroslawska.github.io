98090dd122f7392a51aa10356b3a9a723896b2fa
Marta <msieroslawska@users.noreply.github.com>
2020-07-20 21:41:02 +0200

    Chore: regen site

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f7787759aab2b3f2e636";
+/******/ 	var hotCurrentHash = "39a85aabf40c03990250";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
