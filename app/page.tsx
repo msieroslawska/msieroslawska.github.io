1c0252b0f4f2642e828c360913dee34739d52608
Marta <msieroslawska@users.noreply.github.com>
2025-07-09 22:48:23 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,94 @@
-export default function HomePage() {
-  return <div>Home page</div>;
+import {
+  Anchor,
+  AppShell,
+  AppShellFooter,
+  AppShellHeader,
+  AppShellMain,
+  Center,
+  Container,
+  Text,
+  Title,
+} from "@mantine/core";
+// import * as contentful from "contentful";
+
+// function test() {
+//   const client = contentful.createClient({
+//     space: process.env.CONTENTFUL_SPACE_ID || "",
+//     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+//   });
+
+//   client
+//     .getEntries({
+//       content_type: "codeLog",
+//     })
+//     .then((response) => {
+//       console.log(response.items);
+//     });
+// }
+
+export default function Home() {
+  return (
+    <AppShell header={{ height: 60 }} footer={{ height: 60 }}>
+      <AppShellHeader>
+        <Container>
+          <Title order={2}>Hi!</Title>
+          <Text size="sm" color="dimmed">
+            A modern template using Mantine UI, Next.js, and Bun.
+          </Text>
+        </Container>
+      </AppShellHeader>
+      <AppShellMain>
+        <Container p="xl" mt={200}>
+          <Text mt="md">
+            <strong>
+              <Anchor
+                href="https://mantine.dev/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Mantine UI
+              </Anchor>
+            </strong>
+            : A comprehensive and customizable UI component library.
+          </Text>
+          <Text>
+            <strong>
+              <Anchor
+                href="https://nextjs.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Next.js
+              </Anchor>
+            </strong>
+            : A popular React framework for server-side rendering and static
+            site generation.
+          </Text>
+          <Text>
+            <strong>
+              <Anchor
+                href="https://bun.sh/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Bun
+              </Anchor>
+            </strong>
+            : A fast and efficient package manager, bundler, and test runner.
+          </Text>
+        </Container>
+      </AppShellMain>
+      <AppShellFooter>
+        <Center h={60}>
+          <Text>
+            Page built using{" "}
+            <Anchor href="https://contentful.com">Contentful</Anchor>,{" "}
+            <Anchor href="https://mantine.dev">Mantine</Anchor> and{" "}
+            <Anchor href="https://nextjs.org">Next.js</Anchor> &middot;{" "}
+            <Anchor href="https://github.com/msieroslawska/">Source</Anchor>
+          </Text>
+        </Center>
+      </AppShellFooter>
+    </AppShell>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
